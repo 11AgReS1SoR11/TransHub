a7b9cda77f0e43a1ce734ebc65614ceb9ade8c5b
maxonic <kromachmax2003@gmail.com>
2024-03-09 18:06:47 +0300

    fixed bags

--- GuiApplication/objects/exiticon.cpp
+++ GuiApplication/objects/exiticon.cpp
@@ -1,3 +1,12 @@
 #include "objects/exiticon.h"
 
 
+ExitButton::ExitButton(QWidget *parent):QPushButton(parent) {
+    QIcon icon = QIcon(":/ui/pictures/exit.png");
+    setIcon(icon);
+    setIconSize(QSize(40, 40));
+    setFixedSize(50, 50);
+    // Подключение сигнала нажатия к слоту завершения приложения
+    connect(this, &ExitButton::clicked, qApp, &QApplication::quit);
+}
+
