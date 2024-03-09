61e8470b80807d4faf44ecd9fa9fede6fd550b8a
maxonic <kromachmax2003@gmail.com>
2024-03-09 21:12:19 +0300

    добавлены панели управления приложением

--- GuiApplication/transhubwingow.h
+++ GuiApplication/transhubwingow.h
@@ -8,12 +8,53 @@
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QSplitter>
+#include <QTextEdit>
+#include <QAction>
+#include <QMenu>
+#include <QMenuBar>
+#include <QToolBar>
+#include <QComboBox>
+#include <QCheckBox>
+#include <QPushButton>
+#include <QDockWidget>
+#include <QSettings>
 
 class TransHubWindow: public QMainWindow {
     Q_OBJECT
 public:
     explicit TransHubWindow(QWidget *parent = 0);
+    ~TransHubWindow();
+
+private:
+    LeftScreen* left_screen { nullptr };
+    QAction* newFileAction { nullptr };
+    QAction* openFileAction { nullptr };
+    QAction* saveFileAction { nullptr };
+    QAction* saveAsFileAction { nullptr };
+    QAction* exitProgramAction { nullptr };
+    QToolBar* fileToolBar { nullptr };
+    QToolBar* topToolBar { nullptr };
+    QComboBox* comboBox_1 { nullptr };
+    QComboBox* comboBox_2 { nullptr };
+    QCheckBox* checkBox_1 { nullptr };
+    QDockWidget* dockWidget { nullptr };
+    QTableWidget *tableWidget { nullptr };
+    QPushButton* pushButton { nullptr };
+
+    QMenu* fileMenu { nullptr };
+
+    void elementsPrograms();
+    void writeSettins();
+    void readSettings();
+
+private slots:
+    void newFile();
+    void openFile();
+    void saveFile();
+    void saveAsFile();
+    void exitProgram();
 };
+
 
 #endif // TRANSHUBWINGOW_H
 
