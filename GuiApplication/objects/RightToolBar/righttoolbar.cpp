693a3321088ee72998ba09cfd950b5d5ad1ee4f6
GachiBoy0706 <sazhin.dd@edu.spbstu.ru>
2024-03-12 10:38:10 +0300

    добавил иконки старта/стопа, мб надо переделать

--- GuiApplication/objects/RightToolBar/righttoolbar.cpp
+++ GuiApplication/objects/RightToolBar/righttoolbar.cpp
@@ -10,11 +10,11 @@ RightToolBar::RightToolBar(QToolBar *parent): QToolBar(parent)
 RightToolBar::RightToolBar(const QString &title, QWidget *parent): QToolBar(title, parent)
 {
 
-    build_route = new QAction(tr("GO"), this);
+    build_route = new QAction(QIcon::fromTheme("media-playback-start"),tr("GO"), this);
     build_route->setStatusTip(tr("Building a route"));
     //connect(newFileAction, &QAction::triggered, this, &TransHubWindow::newFile);
 
-    stop_route = new QAction(tr("STOP"), this);
+    stop_route = new QAction(QIcon::fromTheme("media-playback-stop"), tr("STOP"), this);
     stop_route->setStatusTip(tr("Stop the calculation"));
     //connect(openFileAction, &QAction::triggered, this, &TransHubWindow::openFile);
 
