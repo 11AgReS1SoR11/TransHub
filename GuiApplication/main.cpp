e571a89a3933f0db136738c46dfb042400e6f84f
GachiBoy0706 <sazhin.dd@edu.spbstu.ru>
2024-03-14 17:06:22 +0300

    убрал папку include, разобрался в с двумя ворнинграми, с третьим хз

--- GuiApplication/main.cpp
+++ GuiApplication/main.cpp
@@ -5,7 +5,7 @@
 #include <QApplication>
 #include <QWidget>
 #include "transhubwingow.h"
-#include "../include/settings.h"
+
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
